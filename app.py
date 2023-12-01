bfec9113427ca566cb9b3b3fea9779a7adaa28fb
IchrakMaraoub <115190441+IchrakMaraoub@users.noreply.github.com>
2023-12-01 17:04:46 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+products = [
+        {'id': 1, 'name': 'Lipstick', 'brand': 'Maybelline', 'price': '$10.99'},
+        {'id': 2, 'name': 'Mascara', 'brand': 'CoverGirl', 'price': '$8.99'},
+        # Add more products as needed
+    ]
+
+# Existing route for displaying products
+@app.route('/')
+def index():
+    # Cosmetic product data
+    
+    return render_template('index.html', products=products)
+
+# New route for adding a product
+@app.route('/add_product', methods=['POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        brand = request.form['brand']
+        price = request.form['price']
+
+        # Assuming a simple incremental ID
+        new_product = {'id': len(products) + 1, 'name': name, 'brand': brand, 'price': price}
+        products.append(new_product)
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
