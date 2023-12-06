4da99151fe6b36b9b1cc6955c4160a67cc807af9
IchrakMaraoub <115190441+IchrakMaraoub@users.noreply.github.com>
2023-12-06 19:24:33 +0100

    Update and rename tests/test_routes.py to test_module1.py

---
+++ test_module1.py
@@ -0,0 +1,30 @@
+import unittest
+from app import app
+
+class TestAppRoutes(unittest.TestCase):
+
+    def setUp(self):
+        # Set up a test client
+        self.app = app.test_client()
+
+    def test_home_route(self):
+        # Test the home route
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'COSMETIC PRODUCTS', response.data)
+
+    def test_add_product_route(self):
+        # Test the add product route
+        response = self.app.post('/add_product', data=dict(
+            name='New Product',
+            brand='Test Brand',
+            price='$19.99'
+        ), follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'New Product', response.data)
+        self.assertIn(b'Test Brand', response.data)
+        self.assertIn(b'$19.99', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
